1c51c036e9cce30c7f7bf15e171aae32f1523559
arisa-gluegames <142303295+arisa-gluegames@users.noreply.github.com>
2023-11-09 19:11:47 +0800

    Removed unused healthcheck

--- NakamaServer/healthcheck.ts
+++
@@ -1,4 +0,0 @@
-function rpcHealthCheck(ctx: nkruntime.Context, logger: nkruntime.Logger, nk: nkruntime.Nakama, payload: string): string {
-    logger.info("rpcHealthCheck called");
-    return JSON.stringify({success: true});
-}
